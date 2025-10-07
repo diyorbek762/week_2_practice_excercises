5854627ca60006dfa4b61fcdbfb78f06268244e3
Diyorbek <dedamirzayevdiyorbek9@gmail.com>
2025-10-07 11:54:52 +0500

    Add temperature.py, Temperature converter

---
+++ temperature.py
@@ -0,0 +1,11 @@
+temperature = float(input("Please enter the temperature: "))
+
+print(f"\n{'*' * 50}")
+print("                Temperature Conversion")
+print(f"{'*' * 50}\n")
+print(f"Celsius: {temperature}")
+print(f"Farenheit: {temperature * 9 / 5 + 32}")
+print(f'Kelvin: {temperature + 273.15}')
+print(f"\n{'*' * 50}")
+
+
